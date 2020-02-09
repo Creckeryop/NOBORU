0b61af13f9f42d837ac0ca1f9e01f648207668b8
Creckeryop <didager@yandex.ru>
2020-02-09 18:59:32 +0500

    Removing access to not noboru folders

--- source/index.lua
+++ source/index.lua
@@ -10,11 +10,41 @@ local function r_dir(path)
         System.deleteDirectory(path)
     end
 end
+
 if System.checkApp("NOBORUPDT") then
     System.removeApp("NOBORUPDT")
     r_dir("ux0:data/noboru/NOBORU")
 end
+
 System.removeApp = nil
+System.checkApp = nil
+
+local rewrite_dir_fs = {
+    "deleteFile",
+    "deleteDirectory",
+    "doesDirExist",
+    "doesFileExist",
+    "openFile",
+    "listDirectory",
+    "createDirectory",
+    "rename"
+}
+for _, f in ipairs(rewrite_dir_fs) do
+    local old_f = System[f]
+    System[f] = function(...)
+        local path = ({...})[1]
+        if path:find("^ux0:data/noboru/") or path:find("^ux0:/data/noboru/") or path:find("^ux0:/data/noboru$") or path:find("^ux0:data/noboru$") then
+            if not path:find("/%.%.") then
+                return old_f(...)
+            else
+                error(".. in deletion not allowed by me DIO")
+            end
+        else
+            error("no access to dirs lower than noboru folder")
+        end
+    end
+end
+
 local df = dofile
 function loadlib(str)
     df("app0:assets/libs/"..str..".lua")
